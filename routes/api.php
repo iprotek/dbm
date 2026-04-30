3d98586f81e02b6af6367b5ca31274724d67b4c3
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-05-01 01:10:23 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,33 @@ Route::prefix('api')->middleware('api')->name('api')->group(function(){
       //FILE UPLOADS
       //include(__DIR__.'/api/meta-data.php'); 
 
-      Route::get('create-backup',[DbmBackupController::class, 'backup'])->name('.backup');
+      Route::get('create-backup',[
+        "uses"=>[DbmBackupController::class, 'backup'],
+        "description"=>"create backup for database",
+        "is_visible"=>true,
+        "is_allow"=>false
+      ])->name('.backup');
 
-      Route::get('backup-list', [DbmBackupController::class, 'get_list'])->name('.list');
+      Route::get('backup-list', [
+        "uses"=>[DbmBackupController::class, 'get_list'],
+        "description"=>"Get backup list for database",
+        "is_visible"=>true,
+        "is_allow"=>false
+      ])->name('.list');
 
-      Route::post('restore-from-file', [DbmRestoreController::class, 'restore'])->name('.restore');
+      Route::post('restore-from-file', [
+        "uses"=>[DbmRestoreController::class, 'restore'],
+        "description"=>"Restore database from backup file",
+        "is_visible"=>true,
+        "is_allow"=>false
+      ])->name('.restore');
 
-      Route::get('restore-list', [DbmRestoreController::class, 'restore_list'])->name('.restore-list');
+      Route::get('restore-list', [
+        "uses"=>[DbmRestoreController::class, 'restore_list'],
+        "description"=>"Get restore list for database",
+        "is_visible"=>true,
+        "is_allow"=>false
+      ])->name('.restore-list');
 
     });
  
--- routes/manage/dbm.php
+++ routes/manage/dbm.php
@@ -6,7 +6,12 @@ use iProtek\Dbm\Http\Controllers\DbmController;
 Route::prefix('system/dbm')->name('.system.dbm')->group(function(){
 
     //GET APP LISTS BASED ON THE LINK
-    Route::get('', [DbmController::class, 'index']);
+    Route::get('/', [ 
+        "uses"=>[DbmController::class, 'index'],
+        "description"=>"Databse Management Index page",
+        "is_visible"=>false,
+        "is_allow"=>true
+    ]);
     /**My Details */ 
     
 });
