464f0f9dd9c328e08689c2751e26e6b11266bfd4
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-04-30 21:48:07 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use iProtek\Dbm\Http\Controllers\DbmRestoreController;
 
 Route::prefix('api')->middleware('api')->name('api')->group(function(){ 
 
-    Route::prefix('group/{group_id}/system/dbm')->middleware(['pay.api'])->name('.system.dbm')->group(function(){
+    Route::prefix('group/{group_id}/system/dbm')->middleware(['pay.api', 'policy.control'])->name('.system.dbm')->group(function(){
       
       //FILE UPLOADS
       //include(__DIR__.'/api/file-upload.php');
